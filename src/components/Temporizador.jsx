3943547be58f6a4e85515977c1fb83d79f75897c
DiegoRafaelAF <draf0314@gmail.com>
2020-06-09 18:28:41 -0300

    correcting timer

--- src/components/Temporizador.jsx
+++ src/components/Temporizador.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Temporizador extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { time: 0 }
+    this.state = { time: 30 }
     this.stopTimer = this.stopTimer.bind(this);
   }
 
@@ -15,15 +15,15 @@ class Temporizador extends React.Component {
     const time = this.state.time;
     setTimeout(() => {
       this.setState({
-        time: time + 1,
+        time: time - 1,
       })
-      if(time === 30) return this.stopTimer(time);
+      if(time === 0) return this.stopTimer(time);
       return this.timer()
     }, 1000);
   }
 
   stopTimer() {
-    this.setState({ time: 30 })
+    this.setState({ time: 0 })
   }
 
   render() {
