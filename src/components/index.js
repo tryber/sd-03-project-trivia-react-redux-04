5cc5478bd0e18d9629ed140a23d91d31aa2267a9
Gustaff <gustavohmfigueiredo@gmail.com>
2020-06-09 14:39:10 -0300

    taking out .jsx from index of components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export { default as Home } from './Home.jsx';
-export { default as Game } from './Game.jsx';
-export { default as Feedback } from './Feedback.jsx';
-export { default as Ranking } from './Ranking.jsx';
+export { default as Home } from './Home';
+export { default as Game } from './Game';
+export { default as Feedback } from './Feedback';
+export { default as Ranking } from './Ranking';
 export { default as Config } from './Config';
 export { default as PageNotFound } from './PageNotFound';
