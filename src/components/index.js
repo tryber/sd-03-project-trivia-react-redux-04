a50c8231adb9319a24116e1830a82f6961b69c27
Gustaff <gustavohmfigueiredo@gmail.com>
2020-06-09 01:26:55 -0300

    codeclimate

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { default as Home} from './Home.jsx';
-export { default as Game} from './Game.jsx';
+export { default as Home } from './Home.jsx';
+export { default as Game } from './Game.jsx';
 export { default as Feedback } from './Feedback.jsx';
 export { default as Ranking } from './Ranking.jsx';
 export { default as Config } from './Config';
