b3b03905d32758d464af016a151df007110c6fd2
mirellaalves <mirella.alves@gmail.com>
2020-06-09 20:06:59 -0300

    Ranking page button done.

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Ranking = () => (
   <div>
     <h1>This is the <strong>Ranking</strong> page</h1>
+    <Link to={'/'}>
+      <button data-testid="btn-go-home">Voltar ao Início</button>
+    </Link>
   </div>
 );
 
