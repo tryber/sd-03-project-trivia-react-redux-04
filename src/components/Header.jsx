c8e3fed7e0c1f9cfffcc105269c31eda83fbe07f
Gustaff <gustavohmfigueiredo@gmail.com>
2020-06-10 17:35:45 -0300

    quick codeclimate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 
 const Header = () => {
   const { playerName, gravatarEmail } = JSON.parse(localStorage.getItem('state')).player;
@@ -17,17 +15,4 @@ const Header = () => {
   );
 };
 
-Header.propTypes = {
-  playerName: PropTypes.string.isRequired,
-  score: PropTypes.string, // number as string
-  gravatarEmail: PropTypes.string.isRequired,
-};
-
-Header.defaultProps = {
-  score: null
-};
-
-const mapStateToProps = (state) => ({
-});
-
-export default connect(mapStateToProps)(Header);
+export default Header;
