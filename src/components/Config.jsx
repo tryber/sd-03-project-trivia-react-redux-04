5c4381fbe2fbce7be815b77996638d164ded26c6
DiegoRafaelAF <draf0314@gmail.com>
2020-06-10 18:26:12 -0300

    create page configuration game

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -1,32 +1,74 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-const category = [];
+const category = [
+  'General Knowledge',
+  'Entertainment: Books',
+  'Entertainment: Film',
+  'Entertainment: Music',
+  'Entertainment: Musicals & Theatres',
+  'Entertainment: Television',
+  'Entertainment: Video Games',
+  'Entertainment: Board Games',
+  'Science & Nature',
+  'Science: Computers',
+  'Science: Mathematics',
+  'Mythology',
+  'Sports',
+  'Geography',
+  'History',
+  'Politics',
+  'Art',
+  'Celebrities',
+  'Animals',
+  'Vehicles',
+  'Entertainment: Comics',
+  'Science: Gadgets',
+  'Entertainment: Japanese Anime & Manga',
+  'Entertainment: Cartoon & Animations',
+];
 const difficulty = ['easy', 'medium', 'hard'];
-const type = [];
+const type = ['multiple', 'boolean'];
 
-const Config = () => (
-  <div>
-    <h1 data-testid="settings-title">Configurações</h1>
-    <div>
-      <select>
-        Categoria
+export class Config extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: '',
+      difficulty: '',
+      type: '',
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1 data-testid="settings-title">Configurações</h1>
+        <div>
+          <select onChange={(e) => this.setState({ category: e.target.value})}>
+            Categoria
         {category.map((e) => <option key={e}>{e}</option>)}
-      </select>
-      <select>
-        Dificuldade
+          </select>
+          <select onChange={(e) => this.setState({ difficulty: e.target.value})}>
+            Dificuldade
         {difficulty.map((e) => <option key={e}>{e}</option>)}
-      </select>
-      <select>
-        Tipo
+          </select>
+          <select onChange={(e) => this.setState({ type: e.target.value})}>
+            Tipo
         {type.map((e) => <option key={e}>{e}</option>)}
-      </select>
-    </div>
-  </div>
-);
+          </select>
+        </div>
+      </div>
+    );
+  }
+}
 
-const mapStateToProps = ({ game: { questionID }, APIQuestions: { questions } }) => ({
-  question: questions[questionID],
-});
+const mapStateToProps = (state) => ({
 
-export default connect(mapStateToProps)(Config);
+})
+
+const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Config)
+
