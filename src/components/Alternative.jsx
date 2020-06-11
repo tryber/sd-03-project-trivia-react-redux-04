01497cd7b4d7e5f73b36f90bbe7689391098caa0
Gustaff <gustavohmfigueiredo@gmail.com>
2020-06-11 17:08:22 -0300

    codeclimate

--- src/components/Alternative.jsx
+++ src/components/Alternative.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { onAnswerdQuestion, stopTime } from '../actions/game';
+import { onAnswerdQuestion } from '../actions/game';
 
 import { setScore } from '../services/localStorageAPI';
 
