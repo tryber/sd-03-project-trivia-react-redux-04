9308d5eca5d013c3cce2f56f49c2fd6cd38be866
DiegoRafaelAF <draf0314@gmail.com>
2020-06-15 19:07:40 -0300

    alteration message feedback

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -13,6 +13,7 @@ const feedbackText = (assertions) => {
 const Feedback = () => {
   const state = JSON.parse(localStorage.getItem('state')) || INITIAL_STORAGE_STATE;
   const { score, assertions } = state.player;
+  console.log(score)
   return (
     <div className="content">
       <div>
@@ -22,8 +23,8 @@ const Feedback = () => {
         <h1><strong>Feedback</strong></h1>
         <div>
           <h3 data-testid="feedback-text">{feedbackText(assertions)}</h3>
-          <h4 data-testid="feedback-total-score">Pontuação {score}</h4>
-          <h4 data-testid="feedback-total-question">Acertos {assertions}</h4>
+          <h4>Pontuação <span data-testid="feedback-total-score">{score}</span></h4>
+          <h4>Acertos <span data-testid="feedback-total-question">{assertions}</span></h4>
         </div>
         <Link to="/">
           <button
