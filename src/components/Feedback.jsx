384fe41c735092a84e5402c8b5ef8757e4843178
DiegoRafaelAF <draf0314@gmail.com>
2020-06-15 16:47:56 -0300

    Feedback Page informations

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
+import './CSS_Components/Feedback.css';
+
+const { score, assertions } = JSON.parse(localStorage.getItem('state')).player;
+const feedbackText = () => {
+  if (assertions < 3) return 'Podia ser melhor...';
+  if (assertions >= 3) return 'Mandou bem!';
+  return null;
+};
 
 const Feedback = () => (
-  <div>
+  <div className="content">
     <div>
       <Header shouldShowScore />
     </div>
-    <h1>This is the <strong>Feedback</strong> page</h1>
-    <Link to="/">
-      <button data-testid="btn-play-again">Jogar Novamente</button>
-    </Link>
-    <Link to={'/ranking'}>
-      <button data-testid="btn-ranking">Ver Ranking</button>
-    </Link>
+    <div className="feedback-component">
+      <h1><strong>Feedback</strong></h1>
+      <div>
+        <h3 data-testid="feedback-text">{feedbackText()}</h3>
+        <h4 data-testid="feedback-total-score">Pontuação {score}</h4>
+        <h4 data-testid="feedback-total-question">Acertos {assertions}</h4>
+      </div>
+      <Link to="/">
+        <button
+          className="btn btn-outline-dark"
+          data-testid="btn-play-again"
+        >
+          Jogar Novamente
+        </button>
+      </Link>
+      <Link to={'/ranking'}>
+        <button className="btn btn-outline-danger" data-testid="btn-ranking">Ver Ranking</button>
+      </Link>
+    </div>
   </div>
 );
 
