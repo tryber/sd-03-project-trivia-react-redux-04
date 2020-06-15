759bd29a65bc327f27a56b8775d9dc76e093445a
DiegoRafaelAF <draf0314@gmail.com>
2020-06-15 17:38:52 -0300

    feedback

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -3,13 +3,25 @@ import { Link } from 'react-router-dom';
 import Header from './Header';
 import './CSS_Components/Feedback.css';
 
+const { score, assertions } = JSON.parse(localStorage.getItem('state')).player;
+const feedbackText = () => {
+  if (assertions < 3) return 'Podia ser melhor...';
+  if (assertions >= 3) return 'Mandou bem!';
+  return null;
+};
+
 const Feedback = () => (
   <div className="content">
     <div>
       <Header shouldShowScore />
     </div>
     <div className="feedback-component">
-      <h1>This is the <strong>Feedback</strong> page</h1>
+    <h1><strong>Feedback</strong></h1>
+      <div>	      <div>
+        <h3 data-testid="feedback-text">{feedbackText()}</h3>
+        <h4 data-testid="feedback-total-score">Pontuação {score}</h4>
+        <h4 data-testid="feedback-total-question">Acertos {assertions}</h4>
+      </div>	      </div>
       <Link to="/">
         <button
           className="btn btn-outline-dark"
